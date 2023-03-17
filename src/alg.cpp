4fa56728fec11d6c78132c00b70aa8698f5a8975
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-03-17 11:50:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int right = size, left = 0, mid = (right + left) / 2;
+int count = 0, i = 0, sizeDiv = size;
+while (right-left>1) {
+mid = (right + left) / 2;
+if (arr[mid] > value) {
+sizeDiv /= 2;
+if (sizeDiv < 1) sizeDiv += 1;
+right -= sizeDiv;
+}
+else if (arr[mid] < value) {
+sizeDiv /= 2;
+if (sizeDiv < 1) sizeDiv +=1;
+left += sizeDiv;
+}
+else if (arr[mid] == value) {
+while (arr[mid + i] == value) {
+count++;
+i++;
+};
+i = 1;
+while (arr[mid - i] == value) {
+count++;
+i++;
+};
+return count;
+}
+}
+return 0;
 }
